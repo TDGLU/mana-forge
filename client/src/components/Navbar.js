9fba808c5455bfadfcb7b334c3b97f3928bc5a49
TDGLU <tdglukealt@gmail.com>
2023-03-09 21:59:44 -0800

    add some default styles to the search section and navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,24 +4,12 @@ import Popup from 'reactjs-popup';
 import Login from '../pages/Login';
 import SignUp from '../pages/SignUp';
 
-const styles ={
-  nav:{
-   backgroundColor: 'green',
-   textDecorationColor: 'white',
-   display: 'flex',
-   justifyContent: 'space-between',
-   alignItems: 'center',
-   gap : '2rem',
-   padding: '0 1rem',
-  }
-}
-    
-
 function Navbar() {
   return (
     <>
-      <nav style={styles.nav}>
-      <Link to ='/'>Home</Link>
+      <nav
+      className='navbar' >
+      <Link className='home-btn' to ='/'>Home</Link>
         <ul>
           <li>
             <Link to ='/featured'>Featured</Link>
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -3,17 +3,17 @@ import React, { useState } from 'react'
 function SearchBar(props) {
 
     return (
-        <div>
+        <div className='search-section'>
             <form>
                 <label for="card-name">Enter card name:</label>
-                <input 
+                <input className='form-interact'
                     type="text" 
                     id="card-name" 
                     name="card-name" 
                     value={props.cardName}
                     onChange={e => props.setCardName(e.target.value)}
                     />
-                <button type="button" onClick={(e) => props.searchForCard(e)}>Search</button>
+                <button className='form-interact search-btn' type="button" onClick={(e) => props.searchForCard(e)}>Search</button>
             </form>
         </div>
     )
