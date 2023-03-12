3f166771e13eb59b88b12fd98fb02915cae2a723
TDGLU <tdglukealt@gmail.com>
2023-03-11 21:13:55 -0800

    updating the styles and pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='loginSignUpContainer'>
+      <div className='loginSignUpTitle'>Login</div>
+      <input className='usernameInput' placeholder='Username'></input>
+      <input className='passwordInput' placeholder='Password'></input>
+      <button className='signUpLoginButton'>Login</button>
+    </div>
+
   )
 }
 
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
+import './SignUpLogin.css';
+
 function SignUp() {
   return (
-    <div>Sign Up</div>
+    <div className='loginSignUpContainer'>
+      <div className='loginSignUpTitle'>Sign Up</div>
+      <input className='usernameInput' placeholder='Username'></input>
+      <input className='emailInput' placeholder='Email'></input>
+      <input className='passwordInput' placeholder='Password'></input>
+      <button className='signUpLoginButton'>Sign Up</button>
+    </div>
   )
 }
 
