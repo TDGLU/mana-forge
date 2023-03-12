452c46f45d31dbc983508f935eb6564e2287be4a
TDGLU <tdglukealt@gmail.com>
2023-03-12 01:37:44 -0800

    login signup styling udp

--- client/src/components/CardContainer.js
+++ client/src/components/CardContainer.js
@@ -4,8 +4,10 @@ import React from 'react'
 function CardContainer(props) {
   return (
     <div>
-        <h2>{props.cardData.summary}</h2>
-        <p>{JSON.stringify(props.cardData)}</p>
+        {/* <h2>{props.cardData.summary}</h2> */}
+        <h2>{props.cardData.cardName}</h2>
+        <p className='dataFetch'>{JSON.stringify(props.cardData)}</p>
+        
     </div>
   )
 }
