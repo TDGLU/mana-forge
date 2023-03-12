bb233577fa2d356c959cab8f71548c56832c0480
TDGLU <tdglukealt@gmail.com>
2023-03-12 04:26:28 -0700

    fixed bug that uppercasing rarity will crash app

--- client/src/components/CardContainer.js
+++ client/src/components/CardContainer.js
@@ -7,6 +7,10 @@ function CardContainer(props) {
   // console.log(props.cardData.artist)
   // console.log(props.cardData.image_uris.normal)
 
+// Bug: Uppercasing rarity will crash app 
+  // const rarity = props?.cardData.rarity
+  // const rarityFinal = rarity.charAt(0).toUpperCase() + rarity.slice(1)
+
   return (
     <div className='card-container'>
         {/* <h2>{props.cardData.summary}</h2> */}
@@ -18,16 +22,25 @@ function CardContainer(props) {
           <img src={props.cardData.image_uris ? props.cardData.image_uris?.normal : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/1024px-Question_mark_%28black%29.svg.png"} alt="card image" />
 
           {/* Title of card */}
-          <p>{props?.cardData.name}</p>
+          <p><b>{props?.cardData.name}</b></p>
 
           {/* Author  */}
           <p>Author: <span>{props?.cardData.artist}</span></p>
+
+          {/* Collector Number */}
+          <p>Collector Number: <span>{props?.cardData.collector_number }</span></p>
 
           {/* Rarity  */}
           <p>Rarity: <span>{props?.cardData.rarity}</span></p>
 
           {/* Tough */}
-          <p>toughness: <span>{props?.cardData.toughness }</span></p>
+          <p>Toughness: <span>{props?.cardData.toughness }</span></p>
+
+          {/* Type */}
+          <p>Type: <span>{props?.cardData.type_line }</span></p>
+
+          {/* Release Date */}
+          <p>Release Date: <span>{props?.cardData.released_at }</span></p>
         </div>
 
         {/* <p className='dataFetch'>{JSON.stringify(props.cardData)}</p>  */}
